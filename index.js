dd364e29fe784b895935169918606c7718e535d3
NokukhanyaPetra <113669556+NokukhanyaPetra@users.noreply.github.com>
2023-12-29 17:12:29 +0200

    Api intergration, update the correct weather of a place

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "f32bcobff3tf69a32a4c45cab6e111b0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handlesubmitSearch(event) {
   event.preventDefault();
   let cityPlace = document.querySelector("#city-info");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = cityPlace.value;
+  searchCity(cityPlace.value);
 }
 
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", handlesubmitSearch);
+searchCity("Potchefstroom");
